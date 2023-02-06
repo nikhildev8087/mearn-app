fa01c9c73fcfeb3b434196e7da0cfdb04a916e6c
Nikhil Borge <nikhil.b@prometteur.in>
2023-02-06 16:18:51 +0530

    frontend added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -6,9 +6,9 @@ const Goal = require('../models/goalModel');
 //@access Private
 
 const getGoals = asyncHandler(async(req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json(goals)
-    res.status(200).json({message: 'Get Goals'})
+    // res.status(200).json({message: 'Get Goals'})
 })
 
 //@desc Set goals
@@ -24,6 +24,7 @@ const setGoals = asyncHandler(async(req, res) => {
 
     const goal = await Goal.create({
         text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -40,9 +41,23 @@ const updateGoals = asyncHandler(async(req, res) => {
         throw new Error('Goal not found')
     }
 
+    // const user = await User.findById(req.user.id)
+
+    //Check for user 
+    if(!req.user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //Make sure the logged in user matches the goal user
+    if(goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoals = await Goal.findByIdAndUpdate(req.params.id, req.
         body, {
-            new:true,
+            new:true, 
     })
 
     
@@ -60,6 +75,21 @@ const deleteGoals = asyncHandler(async(req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    // const user = await User.findById(req.user.id)
+
+    //Check for user 
+    if(!req.user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //Make sure the logged in user matches the goal user
+    if(goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     await goal.remove()
 
     res.status(200).json({id: req.params.id})
@@ -68,7 +98,7 @@ const deleteGoals = asyncHandler(async(req, res) => {
 
 
 module.exports = {
-    getGoals, setGoals,updateGoals, deleteGoals
+    getGoals, setGoals, updateGoals, deleteGoals
 }
 
 
